c4ea88c016d2bc238ac6a59a1473cbe29af2eb21
Yazan2006-coder <1218454@student.roc-nijmegen.nl>
2026-05-28 14:41:13 +0200

    navigation color changes when clicked on

--- header.php
+++ header.php
@@ -5,10 +5,10 @@
                 <div class="logo-tagline">JOUW WERELD, JOUW KOERS</div>
             </div>
             <nav class="nav">
-                <a href="index.php" class="nav-active">Home</a>
-                <a href="reizen.php">Reizen</a>
-                <a href="overons.php">Over ons</a>
-                <a href="contact.php">Contact</a>
+                <a href="index.php" class="<?php echo (basename($_SERVER['PHP_SELF']) == 'index.php') ? 'nav-active' : ''; ?>">Home</a>
+                <a href="reizen.php" class="<?php echo (basename($_SERVER['PHP_SELF']) == 'reizen.php') ? 'nav-active' : ''; ?>">Reizen</a>
+                <a href="overons.php" class="<?php echo (basename($_SERVER['PHP_SELF']) == 'overons.php') ? 'nav-active' : ''; ?>">Over ons</a>
+                <a href="contact.php" class="<?php echo (basename($_SERVER['PHP_SELF']) == 'contact.php') ? 'nav-active' : ''; ?>">Contact</a>
             </nav>
             <div class="auth-buttons">
                 <a href="#" class="btn-login">Inloggen</a>
