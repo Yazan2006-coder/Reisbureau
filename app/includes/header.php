7ef25257f4601e6b3a11abfb7f8b7ca60f975936
Yazan Uqla <1218454@student.roc-nijmegen.nl>
2026-06-15 16:48:32 +0200

    navigeren vanuit admin pagina probleem opgelost

--- app/includes/header.php
+++ app/includes/header.php
@@ -5,19 +5,19 @@
                 <div class="logo-tagline">JOUW WERELD, JOUW KOERS</div>
             </div>
             <nav class="nav">
-                <a href="index.php" class="<?php echo (basename($_SERVER['PHP_SELF']) == 'index.php') ? 'nav-active' : ''; ?>">Home</a>
-                <a href="reizen.php" class="<?php echo (basename($_SERVER['PHP_SELF']) == 'reizen.php') ? 'nav-active' : ''; ?>">Reizen</a>
-                <a href="overons.php" class="<?php echo (basename($_SERVER['PHP_SELF']) == 'overons.php') ? 'nav-active' : ''; ?>">Over ons</a>
-                <a href="contact.php" class="<?php echo (basename($_SERVER['PHP_SELF']) == 'contact.php') ? 'nav-active' : ''; ?>">Contact</a>
+                <a href="/index.php" class="<?php echo (basename($_SERVER['PHP_SELF']) == 'index.php') ? 'nav-active' : ''; ?>">Home</a>
+                <a href="/reizen.php" class="<?php echo (basename($_SERVER['PHP_SELF']) == 'reizen.php') ? 'nav-active' : ''; ?>">Reizen</a>
+                <a href="/overons.php" class="<?php echo (basename($_SERVER['PHP_SELF']) == 'overons.php') ? 'nav-active' : ''; ?>">Over ons</a>
+                <a href="/contact.php" class="<?php echo (basename($_SERVER['PHP_SELF']) == 'contact.php') ? 'nav-active' : ''; ?>">Contact</a>
             </nav>
             <div class="auth-buttons">
                 <?php if (isset($_SESSION['gebruiker_id'])): ?>
                     <span class="header-welkom">Hallo, <?= htmlspecialchars($_SESSION['gebruiker_naam']) ?></span>
-                    <a href="mijn-account.php" class="btn-login">Mijn Account</a>
-                    <a href="logout.php" class="btn-login">Uitloggen</a>
+                    <a href="/mijn-account.php" class="btn-login">Mijn Account</a>
+                    <a href="/logout.php" class="btn-login">Uitloggen</a>
                 <?php else: ?>
-                    <a href="login.php" class="btn-login">Inloggen</a>
-                    <a href="register.php" class="btn-signup">Account aanmaken</a>
+                    <a href="/login.php" class="btn-login">Inloggen</a>
+                    <a href="/register.php" class="btn-signup">Account aanmaken</a>
                 <?php endif; ?>
             </div>
         </div>
