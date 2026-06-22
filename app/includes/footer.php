54fd1079cd67c6554f8406c29657e35a2e3d5a3b
Yazan Uqla <1218454@student.roc-nijmegen.nl>
2026-06-22 16:25:55 +0200

    footer links to buttons

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -1,12 +1,12 @@
     <footer class="footer">
         <div class="footer-container">
             <p>Horizont Reizen - Nijmegen</p>
-            <ul class="footer-links">
-                <li><a href="/reizen.php">Reizen</a></li>
-                <li><a href="/contact.php">Contact</a></li>
-                <li><a href="/overons.php">Over ons</a></li>
-                <li><a href="/guidelines.php">Algemene voorwaarden</a></li>
-            </ul>
+            <div class="footer-links">
+                <a href="/reizen.php" class="btn">Reizen</a>
+                <a href="/contact.php" class="btn">Contact</a>
+                <a href="/overons.php" class="btn">Over ons</a>
+                <a href="/guidelines.php" class="btn">Algemene voorwaarden</a>
+            </div>
             <p>info@horizont.nl - 030 123 45 67</p>
         </div>
     </footer>
